c73a5f2683044a0ce00ba7592d954f5d98dcedf0
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 01:38:34 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,11 @@ ORDER BY SUM(pl.amount), u.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT pr.category, SUM(pl.amount)
+FROM projects AS pr
+LEFT JOIN pledges AS pl ON pr.id = pl.project_id
+WHERE pr.category = 'music'
+GROUP BY pr.category;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
