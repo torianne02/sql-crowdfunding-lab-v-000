4393ceafe473e646374aafdd82da590420fd0dbb
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-28 01:04:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,8 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.amount
 FROM  projects
-LEFT JOIN pledges ON projects.id = pledges.project_id"
+LEFT JOIN pledges ON projects.id = pledges.project_id
+ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
